123060939cf9e1751d8d9416668a7817d2ed03a9
MarkAlperin <mark.t.alperin@gmail.com>
2022-08-01 15:03:43 -0500

    got the date clicker working, still need to see how dates array is indexed/ordered

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+// import dancePuppet from './puppeteerScripts/dance.js';
 
 function App() {
+
+  const clickHandler = () => {
+    console.log('Clicked');
+    // dancePuppet();
+  };
   return (
     <div className="App">
       <p>Hello World</p>
+      {/* <button onClick={clickHandler}>Click me</button> */}
     </div>
   );
 }
