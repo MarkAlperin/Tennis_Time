19331c304d109666ae1022a620bcff92769e595b
MarkAlperin <mark.t.alperin@gmail.com>
2022-08-08 18:26:03 -0500

    fixed date selection

--- src/puppeteerScripts/dance.js
+++ src/puppeteerScripts/dance.js
@@ -1,120 +1,132 @@
+#!/usr/bin/env node
 const cluster = require("cluster");
 const puppeteer = require("puppeteer");
 const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 
 let puppetAttempts = 0;
-const dancePuppet = async (day, court, time) => {
+const months = [
+  "january",
+  "february",
+  "march",
+  "april",
+  "may",
+  "june",
+  "july",
+  "august",
+  "september",
+  "october",
+  "november",
+  "december",
+].reduce((memo, month, indx) => {
+  let daysIndx = indx;
+  if (indx > 6) {
+    daysIndx++;
+  }
+
+  const monthData = {
+    name: month,
+    index: indx,
+    days: !(daysIndx % 2) ? 31 : 30,
+  };
+  if (month === "february") monthData.days = 28;
+  memo[month] = monthData;
+  memo[indx] = monthData;
+  return memo;
+}, {});
+
+const dancePuppet = async (date, court, time) => {
   puppetAttempts++;
+
+  // LAUNCH PAGE *************************************************************
   const browser = await puppeteer.launch({
     headless: false,
     ignoreHTTPSErrors: true,
   });
-  const page = await browser.newPage();
-
-  await page.goto(process.env.SET_LOCATION_URL);
-
-  await page.select("select#facility_num", "25");
-
-  await page.click("input#btnSubmit")
-  await page.waitForSelector('a[href="SignIn"]').catch((e) => console.error(e));
-
-
-    page.$eval('a[href="SignIn"]', (el) => el.click())
-
-  await page
-    .waitForSelector('input[id="user_id"]')
-    .catch((e) =>
-      console.error(
-        e.message,
-        "USERNAME: ",
-        process.env.USERNAME,
-        "PASSWORD: ",
-        process.env.PASSWORD
-      )
-    );
-
-  await page.type('input[id="user_id"]', process.env.USERNAME);
-  await page.type('input[id="password"]', process.env.PASSWORD);
-
-  await Promise.all([
-    page.click('input[id="CheckUser"]'),
-    // page.waitForNavigation({ waitUntil: "networkidle0" }),
-  ]).catch((e) =>
-    console.error(
-      e.message,
-      "USERNAME: ",
-      process.env.USERNAME,
-      "PASSWORD: ",
-      process.env.PASSWORD
-    )
-  );
-
-  await page.waitForSelector('input[id="Reservation_Date"]').catch((e) => {
-    if (puppetAttempts < 3) {
-      dancePuppet(day);
-      console.error(
-        e.message,
-        "USERNAME: ",
-        process.env.USERNAME,
-        "PASSWORD: ",
-        process.env.PASSWORD
-      );
+  const errorRetry = async (err) => {
+    console.log("IN ERROR RETRY DUUUUUUUUUDEEEE!");
+    if (2 > puppetAttempts) {
+      await browser.close();
+      await dancePuppet(date, court, time);
+    } else {
+      console.error(err.message);
+      console.log("TOO MANY NO MORE");
     }
-  });
-  console.log(
-    "Setting date ------------------------------------------------------------------------"
-  );
-  let val = await page.$eval('input[id="Reservation_Date"]', (e) => {
-    return e.value;
-  });
-  console.log("val: ", val);
-  await page.focus('input[id="Reservation_Date"]');
-  // await page.type('input[id="Reservation_Date"]', "7/31/2022");
-  await page.click('img[class="ui-datepicker-trigger"]');
-  await page.waitForSelector('a[class="ui-state-default"]');
+  };
+  const page = await browser.newPage();
+  await page.goto(process.env.SET_LOCATION_URL)//.catch((e) => errorRetry(e));
+
+  // SET LOCATION *************************************************************
+  await page.select("select#facility_num", "25")//.catch((e) => errorRetry(e));
+  await page.click("input#btnSubmit")//.catch((e) => errorRetry(e));
+
+  // SIGNING IN  ****************************************************************
+  await page.waitForSelector('a[href="SignIn"]')//.catch((e) => errorRetry(e));
+  await page
+    .$eval('a[href="SignIn"]', (el) => el.click())
+    //.catch((e) => errorRetry(e));
+  await page.waitForSelector('input[id="user_id"]')//.catch((e) => errorRetry(e));
+  await page
+    .type('input[id="user_id"]', process.env.USERNAME)
+   // .catch((e) => errorRetry(e));
+  await page
+    .type('input[id="password"]', process.env.PASSWORD)
+    .catch((e) => errorRetry(e));
+  await page.click('input[id="CheckUser"]')//.catch((e) => errorRetry(e));
+
+  // SELECT DATE *************************************************************
+  await page
+    .waitForSelector('input[id="Reservation_Date"]')
+    //.catch((e) => errorRetry(e));
+  await page.focus('input[id="Reservation_Date"]')//.catch((e) => errorRetry(e));
+  await page
+    .click('img[class="ui-datepicker-trigger"]')
+   // .catch((e) => errorRetry(e));
+  await page
+    .waitForSelector('a[class="ui-state-default"]')
+   // .catch((e) => errorRetry(e));
   let dates = await page
     .$$('a[class="ui-state-default"]', (date) => date)
-    .catch((e) => console.error(e));
+  //  .catch((e) => errorRetry(e));
 
-  // const hrefElement = await page.evaluateHandle(() => document.querySelector('a[class="ui-state-default"]'))
-  // console.log("hrefElement: ", hrefElement.jsonValue());
+  // const dayInt = Number(date.day)
+  // let today = new Date();
+  // let day = dayInt > today.getDate() ? dayInt - 2 : dayInt - 1;
+  dates[Number(date.day) - 2].click()
 
-  // // await hrefElement.click();
 
-  // console.log("dates: ", dates);
-  dates[day].click();
+  // CONFIRM DATE *************************************************************
+  let dateArray = await page
+    .$eval("tr tbody td p b", (e) => {
+      return e.innerText.toLowerCase().replace(/,/g, "").split(" ");
+    })
+    //.catch((e) => errorRetry(e));
+
+  // console.log(months)
 
 
   await page
     .waitForSelector('td[class="open pointer"]')
-    .catch((e) => console.error(e.message));
-  await page.evaluate(({court, time}) => {
-    this.Reserve(court, time);
-  }, {court, time});
+    //.catch((e) => errorRetry(e));
+  await page
+    .evaluate(
+      ({ court, time }) => {
+        this.Reserve(court, time);
+      },
+      { court, time }
+    )
+    //.catch((e) => errorRetry(e));
 
-
-
-
-  await page.waitForSelector('select[id="Duration"]').catch((e) => console.error(e.message));
-  await page.type('input[id="Extended_Desc"]', "I solemnly swear I am up to no good");
-  // await page.evaluate(() => {
-  //   const url = "https://sites.onlinecourtreservations.com/savereservation";
-  //   const postData =
-  //     "Reservation_Date=8%2F13%2F2022&Reservation_Num=&LastBlock=44&Mode=New&From=Reservations&Player_1=RANDI.HEDBERG&Court_Num=1&Start_Time=15&Duration=1&Extended_Desc=";
-  //     console.log(url);
-  //   fetch(url, { method: "POST", body: postData })
-  //     .then((res) => {
-  //       console.log(res);
-  //     })
-  //     .catch((err) => {
-  //       console.log(err.message);
-  //     });
-  // });
-  await page.$eval('input[id="SaveReservation"]', e => e.click());
-
+  await page
+    .waitForSelector('select[id="Duration"]')
+   // .catch((e) => errorRetry(e));
+  await page
+    .type('input[id="Extended_Desc"]', "I solemnly swear I am up to no good")
+   // .catch((e) => errorRetry(e));
+  // await page.$eval('input[id="SaveReservation"]', (e) => e.click());
+  await browser.close();
 };
-const day = 11
+const day = 11;
 
 if (cluster.isMaster) {
   const totalCPUs = require("os").cpus().length;
@@ -124,13 +136,19 @@ if (cluster.isMaster) {
   }
 } else {
   console.log(process.env.dateOffset);
-  const court = 1;
 
-  dancePuppet(day + parseInt(process.env.dateOffset), "1", "14");
+  for (let i = 0; i <= 31; i++) {}
+  const date = { month: "september", day: "11", year: "2022" };
+  //   + parseInt(process.env.dateOffset),
+  dancePuppet(date, "2", "14");
 }
-// dancePuppet();
-// });   Court_Num=1&Start_Time=15&Reservation_Num=&Mode=New&Page=&From=Reservations&Event_Num=&Reservation_Date=8%2F13%2F2022
-
-// A request was made: https://sites.onlinecourtreservations.com/savereservation
-// The request's method is: POST
-// The request's postData is: Reservation_Date=8%2F13%2F2022&Reservation_Num=&LastBlock=44&Mode=New&From=Reservations&Player_1=RANDI.HEDBERG&Court_Num=1&Start_Time=15&Duration=1&Extended_Desc=
+const getMethods = (obj) => {
+  let properties = new Set();
+  let currentObj = obj;
+  do {
+    Object.getOwnPropertyNames(currentObj).map((item) => properties.add(item));
+  } while ((currentObj = Object.getPrototypeOf(currentObj)));
+  return [...properties.keys()].filter(
+    (item) => typeof obj[item] === "function"
+  );
+};
