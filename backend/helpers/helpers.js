a5e57f628ac0ed22db900d11ad04ae8db4f89158
MarkAlperin <mark.t.alperin@gmail.com>
2022-08-18 10:50:14 -0500

    separated helpers to front/backend

---
+++ backend/helpers/helpers.js
@@ -0,0 +1,55 @@
+
+const makeCronString = (date) => {
+  let seconds = date.getSeconds();
+  let minutes = date.getMinutes();
+  let cronString = `${seconds < 50 ? seconds + 10 : seconds - 50} ${
+    seconds < 50 ? minutes : minutes + 1
+  } ${date.getHours()} ${date.getDate()} ${date.getMonth() + 1} * `;
+  return cronString;
+};
+
+const getMethods = (obj) => {
+  let properties = new Set();
+  let currentObj = obj;
+  do {
+    Object.getOwnPropertyNames(currentObj).map((item) => properties.add(item));
+  } while ((currentObj = Object.getPrototypeOf(currentObj)));
+  return [...properties.keys()].filter(
+    (item) => typeof obj[item] === "function"
+  );
+};
+
+const months = [
+  "january",
+  "february",
+  "march",
+  "april",
+  "may",
+  "june",
+  "july",
+  "august",
+  "september",
+  "october",
+  "november",
+  "december",
+].reduce((memo, month, indx) => {
+  let daysIndx = indx;
+  if (indx > 6) {
+    daysIndx++;
+  }
+  const monthData = {
+    name: month,
+    index: indx,
+    days: !(daysIndx % 2) ? 31 : 30,
+  };
+  if (month === "february") monthData.days = 28;
+  memo[month] = monthData;
+  memo[indx] = monthData;
+  return memo;
+}, {});
+
+module.exports = {
+  makeCronString,
+  getMethods,
+  months,
+};
--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1,4 +1,24 @@
-
+const formatResData = (date, time, facility, isRandi) => {
+  date.setHours(time.getHours(), time.getMinutes());
+  const resData = {
+    date: date,
+    time: formatTimeIndex(time),
+    month: months[date.getMonth()].name,
+    day: date.getDate(),
+    facility: facility === "Tennis" ? "25" : "88",
+    courts: facility === "Tennis" ? ["1", "2", "3", "4"] : ["1", "2"],
+    game: facility,
+    humanTime: date.toLocaleString("en-US").split(", "),
+    isRandi: isRandi,
+    isScheduled: false,
+  };
+  let t = resData.humanTime[1];
+  resData.humanTime[1] = `${t.slice(0, t.length - 6)} ${t.slice(
+    t.length - 2,
+    t.length
+  )}`;
+  return resData;
+};
 
 const formatCourtIndex = (courtStr) => {
   const courts = [
@@ -21,115 +41,82 @@ const formatCourtIndex = (courtStr) => {
   }
 };
 
-  const formatTimeIndex = (time) => {
-    const times = [
-      "630",
-      "70",
-      "730",
-      "80",
-      "830",
-      "90",
-      "930",
-      "100",
-      "1030",
-      "110",
-      "1130",
-      "120",
-      "1230",
-      "130",
-      "1330",
-      "140",
-      "1430",
-      "150",
-      "1530",
-      "160",
-      "1630",
-      "170",
-      "1730",
-      "180",
-      "1830",
-      "190",
-      "1930",
-      "200",
-      "2030",
-      "210",
-      "2130",
-    ];
-    const timeStr = `${time.getHours()}${time.getMinutes()}`;
-    if (times.includes(timeStr)) {
-      const tableIndex = times.indexOf(timeStr) + 14;
-      return tableIndex.toString();
-    } else {
-      return null;
-    }
+const formatTimeIndex = (time) => {
+  const times = [
+    "630",
+    "70",
+    "730",
+    "80",
+    "830",
+    "90",
+    "930",
+    "100",
+    "1030",
+    "110",
+    "1130",
+    "120",
+    "1230",
+    "130",
+    "1330",
+    "140",
+    "1430",
+    "150",
+    "1530",
+    "160",
+    "1630",
+    "170",
+    "1730",
+    "180",
+    "1830",
+    "190",
+    "1930",
+    "200",
+    "2030",
+    "210",
+    "2130",
+  ];
+  const timeStr = `${time.getHours()}${time.getMinutes()}`;
+  if (times.includes(timeStr)) {
+    const tableIndex = times.indexOf(timeStr) + 14;
+    return tableIndex.toString();
+  } else {
+    return null;
+  }
+};
+
+const months = [
+  "january",
+  "february",
+  "march",
+  "april",
+  "may",
+  "june",
+  "july",
+  "august",
+  "september",
+  "october",
+  "november",
+  "december",
+].reduce((memo, month, indx) => {
+  let daysIndx = indx;
+  if (indx > 6) {
+    daysIndx++;
+  }
+  const monthData = {
+    name: month,
+    index: indx,
+    days: !(daysIndx % 2) ? 31 : 30,
   };
+  if (month === "february") monthData.days = 28;
+  memo[month] = monthData;
+  memo[indx] = monthData;
+  return memo;
+}, {});
 
-  const getMethods = (obj) => {
-    let properties = new Set();
-    let currentObj = obj;
-    do {
-      Object.getOwnPropertyNames(currentObj).map((item) =>
-        properties.add(item)
-      );
-    } while ((currentObj = Object.getPrototypeOf(currentObj)));
-    return [...properties.keys()].filter(
-      (item) => typeof obj[item] === "function"
-    );
-  };
 
-  const months = [
-    "january",
-    "february",
-    "march",
-    "april",
-    "may",
-    "june",
-    "july",
-    "august",
-    "september",
-    "october",
-    "november",
-    "december",
-  ].reduce((memo, month, indx) => {
-    let daysIndx = indx;
-    if (indx > 6) {
-      daysIndx++;
-    }
-    const monthData = {
-      name: month,
-      index: indx,
-      days: !(daysIndx % 2) ? 31 : 30,
-    };
-    if (month === "february") monthData.days = 28;
-    memo[month] = monthData;
-    memo[indx] = monthData;
-    return memo;
-  }, {});
-
-  const formatResData = (date, time, facility, isRandi) => {
-    date.setHours(time.getHours(), time.getMinutes());
-    const resData = {
-      date: date,
-      time: formatTimeIndex(time),
-      month: months[date.getMonth()].name,
-      day: date.getDate(),
-      facility: facility === "Tennis" ? "25" : "88",
-      courts: facility === "Tennis" ? ["1", "2", "3", "4"] : ["1", "2"],
-      game: facility,
-      humanTime: date.toLocaleString("en-US").split(", "),
-      isRandi: isRandi
-    };
-    let t = resData.humanTime[1]
-    resData.humanTime[1] = `${t.slice(0, t.length-6)} ${t.slice(t.length-2, t.length)}`
-    return resData;
-
-  };
-
-  module.exports = {
-    formatResData,
-    formatTimeIndex,
-    formatCourtIndex,
-    getMethods,
-    months,
-  };
-
+module.exports = {
+  formatResData,
+  formatTimeIndex,
+  formatCourtIndex,
+  months,
+};
