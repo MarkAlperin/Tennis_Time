750ddb267e2715055c7ff97555ff22da274f03a0
MarkAlperin <mark.t.alperin@gmail.com>
2022-08-19 17:37:36 -0500

    test

--- backend/server/index.js
+++ backend/server/index.js
@@ -65,7 +65,7 @@ console.log(`Listening at http://localhost:${process.env.PORT}`);
 
 // const date = new Date();
 // let cronStartString = `${date.getSeconds() + 1} ${date.getMinutes()} ${date.getHours()} ${date.getDate()} ${(date.getMonth() + 1)} * `;
-const cronStartString = "0 37 16 * * *";
+const cronStartString = "0 38 16 * * *";
 cron.schedule(cronStartString, () => {
   console.log("cron trigger line 67 backend/server/index.js", new Date());
   // findAndMakeReservations();
