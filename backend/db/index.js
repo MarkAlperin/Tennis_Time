dd512f93b43029e0934f906d78dcf586f73574b2
MarkAlperin <mark.t.alperin@gmail.com>
2022-08-19 10:47:40 -0500

    changed mongodb connect path

--- backend/db/index.js
+++ backend/db/index.js
@@ -3,12 +3,12 @@ const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 
 mongoose
-.connect(process.env.CONNECT_STRING)
+// .connect(process.env.CONNECT_STRING)
   // .connect(
   //   `mongodb://${process.env.USER}:${process.env.PASSWORD}@${process.env.DB_URL}:${process.env.DB_PORT}/?authMechanism=SCRAM-SHA-256`,
   //   { dbName: `${process.env.DB_NAME}` }
   // )
-  // .connect("mongodb://localhost:27017/reservations")
+  .connect("mongodb://localhost:27017/reservations")
   .then((res) => console.log("You are connected to the Reservations DB!"))
   .catch((err) => console.log(err));
 
