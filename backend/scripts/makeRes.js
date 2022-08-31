9345e2b03b238b5d88738626beedbb3e2541112e
MarkAlperin <mark.t.alperin@gmail.com>
2022-08-31 18:46:42 -0500

    moved all fetch params to variables

--- backend/scripts/makeRes.js
+++ backend/scripts/makeRes.js
@@ -3,13 +3,11 @@ require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 
 const makeReservation = async (res, courtNum) => {
   const dateStrings = res.humanTime[0].split("/");
-  console.log("reservation: ", res)
+
+  const url = "https://sites.onlinecourtreservations.com/savereservation"
   const body = `Reservation_Date=${dateStrings[0]}%2F${dateStrings[1]}%2F${dateStrings[2]}&Reservation_Num=&LastBlock=46&Mode=New&From=Reservations&Player_1=RANDI.HEDBERG&Court_Num=${courtNum}&Start_Time=${res.time}&Duration=2&Reservation_Type=G&Extended_Desc=${res.desc ? res.desc : ""}`
-
-  console.log("SessionCookie: ", res.sessionCookie);
-
-  fetch("https://sites.onlinecourtreservations.com/savereservation", {
-  "headers": {
+  const options = {"body": body, "method": "POST"};
+  const headers = {
     "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
     "accept-language": "en-US,en;q=0.9",
     "cache-control": "no-cache",
@@ -26,14 +24,15 @@ const makeReservation = async (res, courtNum) => {
     "cookie": `ASPSESSIONIDSEDQBSTC=${res.sessionCookie}; device=PC; facility%5Fnum=${res.facility}; user%5Fid=randi%2Ehedberg;`,
     "Referer": "https://sites.onlinecourtreservations.com/Reserve",
     "Referrer-Policy": "strict-origin-when-cross-origin"
-  },
-  "body": body,
-  "method": "POST"
-  }).then(res => {
-    console.log("RAN FETCH...");
-  }).catch(err => {
-    console.error("ERROR RUNNING FETCH: ", err);
-  });
+  }
+
+
+  fetch(url, headers, options)
+    .then(res => {
+      console.log("RAN FETCH...");
+    }).catch(err => {
+      console.error("ERROR RUNNING FETCH: ", err);
+    });
 };
 
 module.exports = makeReservation;
