b0f4abc8b0e487d853ed344b64a3725cba5091a5
MarkAlperin <mark.t.alperin@gmail.com>
2022-09-02 09:21:08 -0500

    cron

--- backend/scripts/findAndMakeReservations.js
+++ backend/scripts/findAndMakeReservations.js
@@ -23,7 +23,7 @@ const findAndMakeReservations = async (options) => {
     const reservationWindowDays = 14.509;
 
     if (!resData.isAttempted && !resData.isReserved && diffDays <= reservationWindowDays) {
-      let cronString = runNow ? helpers.makeCronString(date, runNow) : "30 21 14 * * *";
+      let cronString = runNow ? helpers.makeCronString(date, runNow) : "30 22 14 * * *";
       resData.error = false;
       console.log("resData: ", resData);
       for (let courtNum = 0; courtNum < 1; courtNum++) {
--- backend/server/index.js
+++ backend/server/index.js
@@ -62,7 +62,7 @@ app.listen(process.env.PORT);
 console.log(`Listening at http://localhost:${process.env.PORT}`);
 
 
-cron.schedule("0 20 14 * * *", () => {
+cron.schedule("0 22 14 * * *", () => {
   console.log("RUNNING findAndMakeReservations: ",  new Date());
   findAndMakeReservations({ runNow: false });
 });
