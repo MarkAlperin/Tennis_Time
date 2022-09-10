2958ac2f90629217f7e66d8bd4dfc73e9ebb0565
MarkAlperin <mark.t.alperin@gmail.com>
2022-09-10 08:47:00 -0500

    starting clock 10 mins early

--- backend/server/index.js
+++ backend/server/index.js
@@ -62,7 +62,7 @@ app.listen(process.env.PORT);
 console.log(`Listening at http://localhost:${process.env.PORT}`);
 
 
-cron.schedule("0 59 13 * * *", () => {
+cron.schedule("0 50 13 * * *", () => {
   console.log("RUNNING findAndMakeReservations: ",  new Date());
   findAndMakeRes({ runNow: false });
 });
